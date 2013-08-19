a919e23c8970c28d658967c86d73e67ef89d220e
Chris Reeves <hello@chris.reeves.io>
2013-08-19 17:50:35 +0000

    Ensuring start func is called and Start class is called with correct args

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,6 +8,8 @@
 import mock
 import unittest
 
+from twix.main import run
+
 
 class MainTests(unittest.TestCase):
 
@@ -15,3 +17,19 @@ class MainTests(unittest.TestCase):
         patcher = mock.patch('twix.main.docopt')
         self.docopt = patcher.start()
         self.addCleanup(patcher.stop)
+
+    @mock.patch('twix.main.Start')
+    def test_start_called(self, mock_start):
+        """ Ensure twix.main.start is called when start command
+        is supplied to docopt.
+        """
+
+        args = {
+            'start': True,
+            '<name>': 'foo'
+        }
+
+        self.docopt.return_value = args
+        run()
+
+        mock_start.assert_called_with('foo')
