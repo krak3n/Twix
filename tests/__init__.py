c3d56581a0179d2f2f7547f0527be4c73726a468
Chris Reeves <hello@chris.reeves.io>
2013-08-19 17:40:50 +0000

    Stubbed out basic test suite

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: tests
+   :synopsis: Tests for Twix application
+"""
---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: tests.test_main
+   :synopsis: Tests for twix.main
+"""
+
+import unittest
+
+
+class MainTests(unittest.TestCase):
+
+    pass
